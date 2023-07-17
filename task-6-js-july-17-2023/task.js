6215eae10d3f41753a9222d553393f790939fa87
Hasan-abu-khalil <hasan.aak1998@gmail.com>
2023-07-17 21:49:28 +0300

    hello

--- task-6-js-july-17-2023/task.js
+++ task-6-js-july-17-2023/task.js
@@ -32,31 +32,6 @@ x.send()
 
 
 
-// let htt = new XMLHttpRequest();
-// htt.open("GET", "task.json")
-// button.onclick = function () {
-//     let ahmad;
-//     const httjs = JSON.parse(htt.responseText);
-//     for (let i = 0; i < 3; i++) {
-//         ahmad += ` <tr >
-//    <td>${httjs.names[i]}</td>
-//    <td>${httjs.Age[i]}</td>
-//    <td>${httjs.major[i]} </td>
-//    <td>${httjs.unvirsity[i]}</td>
-//    <td>
-// <img src="${httjs.images[i]}" alt="">
-//    </td>
-//  </tr>
-//  <br>`
-
-
-//     }
-//     tbody.innerHTML = ahmad
-
-// }
-// htt.send();
-
-
 
 
 
