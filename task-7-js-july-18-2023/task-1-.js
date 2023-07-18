1248fd08e7af5f43a3b4a2881fdc12ee7c70bd6a
Hasan-abu-khalil <hasan.aak1998@gmail.com>
2023-07-18 16:15:36 +0300

    hello

--- task-7-js-july-18-2023/task-1-.js
+++ task-7-js-july-18-2023/task-1-.js
@@ -1,16 +1,16 @@
 async function get() {
-    const response = await fetch("https://api.github.com/users/hasan-abu-khalil")
+    const response = await fetch("https://api.github.com/users/hasan-abu-khalil/repos")
     const data = await response.json()
 
     const cards = document.getElementById('cards')
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < data.length; i++) {
         cards.innerHTML += `
         
         <div>
-        <h1>${data.login}</h1>
-        <h3>${data.id}</h3>
-        <h4>${data.node_id}</h4>
-        <h4>${data.type}</h4>
+        <h3>${data[i].full_name}</h3>
+        <h3>${data[i].name}</h3>
+        <h3>${data[i].id}</h3>
+        <h4>${data[i].node_id}</h4>
         </div>
         
 
